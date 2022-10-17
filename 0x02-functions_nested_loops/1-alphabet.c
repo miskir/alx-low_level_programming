f5b6a64e82a858b73a67136ba537685535da150e
miskir <miskirn@gmail.com>
2022-10-17 08:16:00 +0000

    function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\');
+}
