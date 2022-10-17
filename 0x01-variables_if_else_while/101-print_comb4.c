02fe2d0966b580c8684b0959addd3b70608840b1
miskir <miskirn@gamil.com>
2022-10-17 04:59:19 +0000

    miskir

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (i = 0; i <= 3; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 1; j <= 3; j++)
 		{
-			for (k = 0; k <= 3; k++)
+			for (k = 2; k <= 3; k++)
 			{
 				printf("%d %d %d\n", i, j, k);
 			}
