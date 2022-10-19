41d046b18ad9583b7269f8671f06560dcda61bde
miskir <miskirn@gamil.com>
2022-10-19 04:54:29 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs -> return absolute value of a number
+ * @n: parameter
+ * Return: bsolute value of number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
